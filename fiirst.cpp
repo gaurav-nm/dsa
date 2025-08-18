117a37c936210a6d54667dcb5e83f44f920a614d
Gaurav <gauravpalstudy@gmail.com>
2025-08-18 19:03:43 +0530

    update for 18 aug

--- fiirst.cpp
+++ fiirst.cpp
@@ -1,7 +1,40 @@
-#include<iostream>
+#include<iostream>    
 using namespace std;
 
+// int main(){
+//     cout << "Hello World " << endl;
+//     cout << "2nd line" << endl;
+
+// int a = 1232;
+// cout << a << endl ;
+//   char b = 'gaurav is on c++ hahahahah';
+//   cout << b << endl ;
+
+//   bool c = 1;
+//   cout << c << endl ;
+
+
+//   float d = 1.2 ;
+//   cout << d << endl;\
+
+
+//   double e = 1.23 ;
+//   cout << e << endl ; 
+
+
+// }
+
+
+
+
 int main(){
-    cout << "Hello World " << endl;
-    cout << "2nd line" << endl;
+unsigned a = 112 ;
+cout << a << endl ;
+
+
+cout << "operators start " << endl ;
+
+double g= 20.12/2 ;
+cout << g << endl ;
+
 }
