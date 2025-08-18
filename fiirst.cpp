10e6da464915e08480e76079d5a3b572f8066083
Gaurav <gauravpalstudy@gmail.com>
2025-08-18 21:59:47 +0530

    modules and operators

--- fiirst.cpp
+++ fiirst.cpp
@@ -1,28 +1,29 @@
 #include<iostream>    
 using namespace std;
+/*
+ int main(){
+     cout << "Hello World " << endl;
+     cout << "2nd line" << endl;
 
-// int main(){
-//     cout << "Hello World " << endl;
-//     cout << "2nd line" << endl;
+ int a = 1232;
+ cout << a << endl ;
+   char b = 'gaurav is on c++ hahahahah';
+   cout << b << endl ;
 
-// int a = 1232;
-// cout << a << endl ;
-//   char b = 'gaurav is on c++ hahahahah';
-//   cout << b << endl ;
-
-//   bool c = 1;
-//   cout << c << endl ;
-
-
-//   float d = 1.2 ;
-//   cout << d << endl;\
+   bool c = 1;
+   cout << c << endl ;
 
 
-//   double e = 1.23 ;
-//   cout << e << endl ; 
+   float d = 1.2 ;
+   cout << d << endl;\
 
 
-// }
+   double e = 1.23 ;
+   cout << e << endl ; 
+
+
+ }
+
 
 
 
@@ -36,5 +37,25 @@ cout << "operators start " << endl ;
 
 double g= 20.12/2 ;
 cout << g << endl ;
+
+
+cout << "Boolean check " << endl ;
+
+int r = 2 ;
+int p = 3;
+
+bool first = (r==p) ;
+cout << first << endl ; 
+}
+
+ */
+
+int main(){
+
+    cout << "modules and operators" << endl ;
+
+    int a = 21 ;
+    cout << !a << endl ;
+    
 
 }
