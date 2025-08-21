7f4907fc70dc47abd4af0acf74682867f90c2d8d
Gaurav <gauravpalstudy@gmail.com>
2025-08-21 20:15:40 +0530

    while loop

---
+++ conditions and loop/whileloop.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+/*
+    int n ;
+    cout << "Enter any number " << endl ;
+    cin >> n ;
+int i ;
+i=1 ;
+
+    while (i<=n)
+
+    {
+        cout << " " << endl ;
+        
+      cout << i << " "  << endl ;
+      i = i+1 ;
+
+    }
+  
+ 
+  
+  cout << "Code to print sum of n numbers " << endl ;
+  int n ;
+  cout << "Enter any number " << endl ;
+  cin >> n ;
+
+  int i , sum = 0;
+  i = 1 ;
+  while(i<=n){
+    sum = sum + i ;
+    i = i + 1;
+   
+  }
+ cout << "The sum of first " << n << " numbers is :  " << sum << endl ;
+*/
+
+}
