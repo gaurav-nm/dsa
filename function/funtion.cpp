041143fe4bb32b4af92986ff99e19e92633d1e54
Gaurav Pal <gauravpalstudy@gmail.com>
2025-09-12 07:45:19 +0530

    funtions

---
+++ function/funtion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std ;
+
+// sum of two numbers
+    int sum (int a , int b ){
+        int s = a + b ;
+        return s ;
+        cout << "Sum of given number is  " << s << endl ;
+
+    }
+
+    int  main (){
+       cout <<  sum (10,5) << endl ;
+
+// cout << 10+15 ;
+        return 0 ;
+
+    }
