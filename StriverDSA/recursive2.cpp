c50dea8356e43e86fc26c6df29e8b380db7ab1ff
Gaurav <gauravpalstudy@gmail.com>
2026-07-29 23:46:42 +0530

    recursion completed

--- StriverDSA/recursive2.cpp
+++ StriverDSA/recursive2.cpp
@@ -34,14 +34,88 @@ void reversearray(vector<int>& arr , int left , int right){
 
 }
 
-int main(){
-    vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
-
-    reversearray(arr, 0 , arr.size()-1);
-
-    for (auto x : arr){
-        cout << x << "  ";
+// Check if the given strin is Palindrome or not 
+bool isPalindrome(string &s, int left , int right ){
+    if (left >= right ){
+        return true;
     }
+    if (s[left] != s[right] ){
+        return false;
+    }
+
+    return isPalindrome(s, left+1 , right -1);
+}
+
+
+// Print fibonacci Series upto Nth term 
+
+int fib(int n){
+    if ( n <= 1 ){
+        return n ;
+    }
+
+    return fib(n-1)+fib(n-2);
+}
+
+
+int main(){
+    // print fibonacci series upto Nth terms 
+    int n = 1000;
+
+    if (n >= 1){
+        cout << 0 << "  ";
+    }
+    if ( n >= 2 ){
+        cout << 1 << "  ";
+    }
+
+    int first = 0;
+    int second = 1;
+
+    for ( int i = 2 ; i < n ; i++ ){
+        int third = first + second;
+
+        cout << third << "  ";
+
+        first = second;
+        second = third;
+
+    }
+    cout << endl ;
+
+
+
+
+
+
+
+
+
+
+    // for (int i = 0 ; i < n; i++){
+    //     cout << fib(i) << " " ;
+        
+    // }
+    // cout << endl ;
+
+
+
+
+
+
+    // string s = "MADAM";
+    // if( isPalindrome(s, 0 , s.size()-1)){
+    //     cout<< "Palindrome";
+    // }
+    // else    cout << "Not Palindrome"<< endl;
+
+    // vector<int> arr = {1,2,3,4,5,6,7,8,9,10};
+
+    // reversearray(arr, 0 , arr.size()-1);
+
+    // for (auto x : arr){
+    //     cout << x << "  ";
+    // }
 
     
 }
