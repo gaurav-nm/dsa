075a0eac71e3e651006014efbb641147b155130d
Gaurav <gauravpalstudy@gmail.com>
2026-08-03 23:58:36 +0530

    Insertion Sort

---
+++ StriverDSA/insertionSort.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+void insertionSort(int arr[], int n ){
+    
+}
+
+int main(){
+
+    int n ;
+    cout << "Enter Array Size: ";
+    cin >> n ;
+    int arr[n];
+
+    for (int i = 0 ; i < n ; i++){
+        cout << "Enter " << i << " index Element :";
+        cin >> arr[i];
+    }
+
+    insertionSort( arr[] , n );
+
+    for(int i = 0 ; i < n ; i++){
+        cout << a[i] ;
+    }
+    return 0 ;
+}
