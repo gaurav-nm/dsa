763ad4b996ba3de258399615857e8ad3affdaf6d
Gaurav <gauravpalstudy@gmail.com>
2026-08-04 00:04:29 +0530

    Insertion Sort complete

--- StriverDSA/insertionSort.cpp
+++ StriverDSA/insertionSort.cpp
@@ -2,7 +2,17 @@
 using namespace std ;
 
 void insertionSort(int arr[], int n ){
-    
+    for(int i = 0 ; i < n ; i++){
+        int key = arr[i];
+        int j = i - 1 ;
+
+        while(j >= 0 && arr[j] > key ){
+            arr[j+1] = arr[j];
+            j--;
+
+        }
+        arr[j+1] = key ;
+    }
 }
 
 int main(){
@@ -17,10 +27,10 @@ int main(){
         cin >> arr[i];
     }
 
-    insertionSort( arr[] , n );
+    insertionSort( arr , n );
 
     for(int i = 0 ; i < n ; i++){
-        cout << a[i] ;
+        cout << arr[i] << " " ;
     }
     return 0 ;
 }
