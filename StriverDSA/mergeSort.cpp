c0ad05fed3884022c2bd8fefafbd67e5ee9cec42
Gaurav <gauravpalstudy@gmail.com>
2026-08-05 00:13:03 +0530

    Merge Sort End

--- StriverDSA/mergeSort.cpp
+++ StriverDSA/mergeSort.cpp
@@ -1,38 +1,78 @@
-#include<bits/stdc++.h>
-using namespace std ;
+#include <bits/stdc++.h>
+using namespace std;
 
-void mergeSort(int arr[], int low , int high){
-    if(low >= high){
-        return ;
-    }
-    int mid = (low + high ) / 2 ;
-    mergeSort( arr, low, mid );
-    mergeSort( arr, mid+1 , high);
+void merge(vector<int>& arr, int low, int mid, int high)
+{
+    vector<int> temp;
 
-    merge(arr, low , mid , high );
-}
-
-void merge(int arr[], int low , int mid , int high ){
-
-    vector<int> temp ;
-
-    int left = low ;
+    int left = low;
     int right = mid + 1;
 
-    while(left <= mid && right <= high ){
-        if(arr[left] < arr[right]){
+    // Merge both sorted halves
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
             temp.push_back(arr[left]);
             left++;
         }
-        else{
+        else
+        {
             temp.push_back(arr[right]);
             right++;
         }
     }
 
-    while(arr[left]< arr[right]){
+    // Copy remaining elements of left half
+    while (left <= mid)
+    {
         temp.push_back(arr[left]);
-        left++
+        left++;
     }
 
+    // Copy remaining elements of right half
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // Copy back to original array
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int>& arr, int low, int high)
+{
+    // Base Case
+    if (low >= high)
+        return;
+
+    // Find Mid
+    int mid = (low + high) / 2;
+
+    // Sort Left Half
+    mergeSort(arr, low, mid);
+
+    // Sort Right Half
+    mergeSort(arr, mid + 1, high);
+
+    // Merge Both Halves
+    merge(arr, low, mid, high);
+}
+
+int main()
+{
+    vector<int> arr = {34, 23, 76, 20, 35};
+
+    mergeSort(arr, 0, arr.size() - 1);
+
+    cout << "Sorted Array: ";
+
+    for (int x : arr)
+        cout << x << " ";
+
+    return 0;
 }
